7a5e84ef0999292bf31bb627f01f6a84307dd918
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-05-15 00:10:42 +0300

    ContentPropertyAttribute ile Items eklendi TabView'lara.

--- MyFinance/Views/ItemsPage.cs
+++ MyFinance/Views/ItemsPage.cs
@@ -7,37 +7,34 @@ public partial class ItemsPage(ItemsPageViewModel viewModel) : FmgLibContentPage
         this
         .Content(
             new TabView()
-            .ItemsSource(
-                new List<TabViewItem>()
-                {
-                    new TabViewItem()
-                    .HeaderText("Mail")
-                    .HeaderTextColor(Black)
-                    .HeaderIcon("home.png")
-                    .HeaderIconColor(Black)
-                    .Content(
-                        new Grid()
-                        .Children(
-                            new Label()
-                            .Text("Mail List")
-                            .Center()
-                        )
-                    ),
-
-                    new TabViewItem()
-                    .HeaderText("Temp")
-                    .HeaderTextColor(Black)
-                    .HeaderIconColor(Black)
-                    .HeaderIcon("home.png")
-                    .Content(
-                        new Grid()
-                        .Children(
-                            new Label()
-                            .Text("Mail List")
-                            .Center()
-                        )
+            .Items_ContentProp(
+                new TabViewItem()
+                .HeaderText("Mail")
+                .HeaderTextColor(Black)
+                //.HeaderIcon("home.png")
+                //.HeaderIconColor(Black)
+                .Content(
+                    new Grid()
+                    .Children(
+                        new Label()
+                        .Text("Mail List")
+                        .Center()
                     )
-                }
+                ),
+
+                new TabViewItem()
+                .HeaderText("Temp")
+                .HeaderTextColor(Black)
+                //.HeaderIconColor(Black)
+                //.HeaderIcon("home.png")
+                .Content(
+                    new Grid()
+                    .Children(
+                        new Label()
+                        .Text("Mail Remt")
+                        .Center()
+                    )
+                )
             )
         );
     }

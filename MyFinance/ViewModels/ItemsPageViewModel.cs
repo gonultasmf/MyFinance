3609f37faf8b22ab2a1c80ebc2f7f312b1245eac
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-05-16 00:51:49 +0300

    collectionview added loadmore.

--- MyFinance/ViewModels/ItemsPageViewModel.cs
+++ MyFinance/ViewModels/ItemsPageViewModel.cs
@@ -2,40 +2,35 @@
 
 public partial class ItemsPageViewModel : BaseViewModel
 {
-    [ObservableProperty]
-    private List<OperatonItemsVM> allItems = new();
+    static List<OperatonItemsVM> AllItems = new List<OperatonItemsVM>();
 
     [ObservableProperty]
-    private List<OperatonItemsVM> inItems = new();
-
-    [ObservableProperty]
-    private List<OperatonItemsVM> outItems = new();
+    private List<OperatonItemsVM> items = new();
 
     [ObservableProperty]
     private List<int> loadingItems = new() { 0,0,0,0,0,0,0,0,0,0,0 };
 
     [ObservableProperty]
-    private bool isLoadingAllItems = false;
+    private bool isLoadingItems = false;
 
     [ObservableProperty]
-    private bool isLoadingInItems = false;
+    private bool isRefreshing = false;
 
     [ObservableProperty]
-    private bool isLoadingOutItems = false;
+    private int operationType = 0;
 
     [ObservableProperty]
-    private int allIType = 0;
+    private int dateType = 3;
 
     [ObservableProperty]
-    private int inIType = 0;
+    private bool isShowPopup = false;
 
-    [ObservableProperty]
-    private int outIType = 0;
+    private int index = 0;
 
     public ItemsPageViewModel()
     {
         Random random = new Random();
-        for (int i = 1; i <= 40; i++)
+        for (int i = 1; i <= 500; i++)
         {
             var amount = random.Next(1, 10000);
             AllItems.Add(
@@ -44,98 +39,74 @@ public partial class ItemsPageViewModel : BaseViewModel
                     Id = Guid.NewGuid(),
                     Icon = amount % 2 == 0 ? "loss.png" : "profits.png",
                     Color = amount % 2 == 0 ? Red : Green,
-                    Date = DateTime.Now.AddDays(-(amount % 30)).ToString("dd.MM.yyyy HH:mm"),
+                    Date = DateTime.Now.AddDays(-(amount % 200)).ToString("dd.MM.yyyy HH:mm"),
                     Title = amount % 2 == 0 ? "Borç ödendi" : "Ödeme Alındı",
                     Description = amount % 2 == 0 ? "Ödemeler yapıldı" : "Yaka parası alındı.",
                     Amount = $"{amount} ₺"
                 }
             );
         }
-        AllItems = AllItems.OrderByDescending(e => DateTime.Parse(e.Date)).ToList();
-        InItems = AllItems.Where(e => e.Color == Green).ToList();
-        OutItems = AllItems.Where(e => e.Color == Red).ToList();
+
+        Items = AllItems.Take(15).ToList();
     }
 
 
-    //[RelayCommand]
-    public List<OperatonItemsVM> GetItems(DateTime date, bool? isIncome = null)
+    public List<OperatonItemsVM> GetItems(DateTime date)
     {
-        IsLoadingAllItems = true;
-
-        List<OperatonItemsVM> tempList = new List<OperatonItemsVM>();
-        Random random = new Random();
-        for (int i = 1; i <= 70; i++)
-        {
-            var amount = random.Next(1, 10000);
-            tempList.Add(
-                new OperatonItemsVM
-                {
-                    Id = Guid.NewGuid(),
-                    Icon = amount % 2 == 0 ? "loss.png" : "profits.png",
-                    Color = amount % 2 == 0 ? Red : Green,
-                    Date = DateTime.Now.AddDays(-(amount % 30)).ToString("dd.MM.yyyy HH:mm"),
-                    Title = amount % 2 == 0 ? "Borç ödendi" : "Ödeme Alındı",
-                    Description = amount % 2 == 0 ? "Ödemeler yapıldı" : "Yaka parası alındı.",
-                    Amount = $"{amount} ₺"
-                }
-            );
-        }
-        tempList = tempList
-            .Where(e => DateTime.Parse(e.Date) >= date && isIncome.HasValue ? isIncome.Value ? e.Color == Green : e.Color == Red : true)
+        var tempList = AllItems
+            .Where(e => DateTime.Parse(e.Date) >= date && OperationType == 0 ? true : OperationType == 1 ? e.Color == Green : e.Color == Red)
             .OrderByDescending(e => DateTime.Parse(e.Date))
+            .Skip(index)
+            .Take(15)
             .ToList();
 
         return tempList;
     }
 
     [RelayCommand]
-    public async Task AllItemTypeChanged()
+    public async Task ApplyFilter()
     {
-        IsLoadingAllItems = true;
-        await Task.Delay(3500);
-        if (AllIType == 0)
-            AllItems = GetItems(DateTime.Now.AddDays(-7));
-        else if (AllIType == 1)
-            AllItems = GetItems(DateTime.Now.AddMonths(-1));
-        else if (AllIType == 2)
-            AllItems = GetItems(DateTime.Now.AddMonths(-6));
-        else if (AllIType == 3)
-            AllItems = GetItems(DateTime.Now.AddYears(-1));
+        IsLoadingItems = true;
+        index = 0;
 
-        IsLoadingAllItems = false;
+        if (DateType == 0)
+            Items = GetItems(DateTime.Now.AddDays(-7));
+        else if (DateType == 1)
+            Items = GetItems(DateTime.Now.AddMonths(-1));
+        else if (DateType == 2)
+            Items = GetItems(DateTime.Now.AddMonths(-6));
+        else if (DateType == 3)
+            Items = GetItems(DateTime.Now.AddYears(-1));
+
+        IsShowPopup = false;
+        await Task.Delay(1500);
+
+        IsLoadingItems = false;
     }
 
     [RelayCommand]
-    public async Task InItemTypeChanged()
+    public async Task LoadMore()
     {
-        IsLoadingInItems = true;
-        await Task.Delay(3500);
-        if (InIType == 0)
-            InItems = GetItems(DateTime.Now.AddDays(-7), true);
-        else if (InIType == 1)
-            InItems = GetItems(DateTime.Now.AddMonths(-1), true);
-        else if (InIType == 2)
-            InItems = GetItems(DateTime.Now.AddMonths(-6), true);
-        else if (InIType == 3)
-            InItems = GetItems(DateTime.Now.AddYears(-1), true);
+        if (index != 0)
+            IsRefreshing = true;
 
-        IsLoadingInItems = false;
+        if (DateType == 0)
+            Items.AddRange(GetItems(DateTime.Now.AddDays(-7)));
+        else if (DateType == 1)
+            Items.AddRange(GetItems(DateTime.Now.AddMonths(-1)));
+        else if (DateType == 2)
+            Items.AddRange(GetItems(DateTime.Now.AddMonths(-6)));
+        else if (DateType == 3)
+            Items.AddRange(GetItems(DateTime.Now.AddYears(-1)));
+
+        //await Task.Delay(1500);
+        index += 15;
+        IsRefreshing = false;
     }
 
     [RelayCommand]
-    public async Task OutItemTypeChanged()
+    public async Task ShowFilterPopup()
     {
-        IsLoadingOutItems = true;
-        await Task.Delay(3500);
-        if (OutIType == 0)
-            OutItems = GetItems(DateTime.Now.AddDays(-7), false);
-        else if (OutIType == 1)
-            OutItems = GetItems(DateTime.Now.AddMonths(-1), false);
-        else if (OutIType == 2)
-            OutItems = GetItems(DateTime.Now.AddMonths(-6), false);
-        else if (OutIType == 3)
-            OutItems = GetItems(DateTime.Now.AddYears(-1), false);
-
-        IsLoadingOutItems = false;
+        IsShowPopup = true;
     }
 }
--- MyFinance/Views/ItemsPage.cs
+++ MyFinance/Views/ItemsPage.cs
@@ -1,5 +1,4 @@
-﻿using FmgLib.MauiMarkup;
-using Microsoft.Maui.Controls.Shapes;
+﻿using Microsoft.Maui.Controls.Shapes;
 using DXImage = DevExpress.Maui.Core.DXImage;
 
 namespace MyFinance.Views;
@@ -10,113 +9,135 @@ public partial class ItemsPage(ItemsPageViewModel viewModel) : FmgLibContentPage
     {
         this
         .Content(
-            new TabView()
-            .HeaderPanelBackgroundColor(DeepSkyBlue)
-            .Items_ContentProp(
-                new TabViewItem()
-                .HeaderText("Tüm İşlemler")
-                .HeaderTextColor(Black)
-                .HeaderFontAttributes(Bold)
+            new Grid()
+            .RowDefinitions(e => e.Star(.7).Star(9.3))
+            .Spacing(10)
+            .Margin(10)
+            .Children(
+                new Grid()
+                .ColumnDefinitions(e => e.Star().Star())
+                .FillHorizontal()
+                .Children(
+                    new Label()
+                    .Text("İŞLEMLER")
+                    .FontAttributes(Bold)
+                    .FontSize(34)
+                    .AlignStart(),
+
+                    new ImageButton()
+                    .Source("filter.png")
+                    .SizeRequest(40,40)
+                    .Command(e => e.Path("ShowFilterPopupCommand"))
+                    .Column(1)
+                    .AlignEnd()
+                ),
+
+                new ShimmerView()
+                .WaveWidth(0.7)
+                .WaveOpacity(0.8)
+                .WaveDuration(new TimeSpan(0, 0, 0, 1))
+                .IsLoading(e => e.Path("IsLoadingItems"))
+                .Row(1)
+                .LoadingView(
+                    new DXCollectionView()
+                    .ItemsSource(e => e.Path("LoadingItems"))
+                    .IsScrollBarVisible(false)
+                    .ItemTemplate(() =>
+                        new Border()
+                        .Margin(5, 5)
+                        .BackgroundColor(LightGray)
+                        .StrokeShape(new RoundRectangle().CornerRadius(25))
+                        .HeightRequest(60)
+                        .StrokeThickness(0)
+                    )
+                )
                 .Content(
-                    new Grid()
-                    .RowDefinitions(e => e.Star(.7).Star(9.3))
-                    .Spacing(10)
-                    .Margin(10)
-                    .Children(
-                        new ComboBoxEdit()
-                        .SelectedIndex(e => e.Path("AllIType"))
-                        .SelectionChangedCommand(e => e.Path("AllItemTypeChangedCommand"))
-                        .ItemsSource(new List<string>
-                        {
-                            "Son 1 Haftalık Veri",
-                            "Son 1 Aylık Veri",
-                            "Son 6 Aylık Veri",
-                            "Son 1 Yıllık Veri"
-                        }),
+                    new DXCollectionView()
+                    .ItemsSource(e => e.Path("Items"))
+                    .IsRefreshing(e => e.Path("IsRefreshing").BindingMode(TwoWay))
+                    .IsLoadMoreEnabled(true)
+                    .LoadMoreCommand(e => e.Path("LoadMoreCommand"))
+                    .IndicatorColor(DeepSkyBlue)
+                    .ItemTemplate(() =>
+                        new Grid()
+                        .RowDefinitions(e => e.Star().Star())
+                        .ColumnDefinitions(e => e.Star(1).Star(6).Star(3))
+                        .Spacing(5)
+                        .Margin(5)
+                        .Children(
+                            new DXImage()
+                            .Source(e => e.Path("Icon"))
+                            .SizeRequest(30, 30)
+                            .RowSpan(2),
 
-                        new ShimmerView()
-                        .WaveWidth(0.7)
-                        .WaveOpacity(0.8)
-                        .WaveDuration(new TimeSpan(0,0,0,1))
-                        .IsLoading(e => e.Path("IsLoadingAllItems"))
-                        .Row(1)
-                        .LoadingView(
-                            new DXCollectionView()
-                            .ItemsSource(e => e.Path("LoadingItems"))
-                            .IsScrollBarVisible(false)
-                            .ItemTemplate(() =>
-                                new Border()
-                                .Margin(5,5)
-                                .BackgroundColor(LightGray)
-                                .StrokeShape(new RoundRectangle().CornerRadius(25))
-                                .HeightRequest(60)
-                                .StrokeThickness(0)
-                            )
-                        )
-                        .Content(
-                            new DXCollectionView()
-                            .ItemsSource(e => e.Path("AllItems"))
-                            .ItemTemplate(() =>
-                                new Grid()
-                                .RowDefinitions(e => e.Star().Star())
-                                .ColumnDefinitions(e => e.Star(1).Star(6).Star(3))
-                                .Spacing(5)
-                                .Margin(5)
-                                .Children(
-                                    new DXImage()
-                                    .Source(e => e.Path("Icon"))
-                                    .SizeRequest(30, 30)
-                                    .RowSpan(2),
+                            new Label()
+                            .FontAttributes(Bold)
+                            .TextColor(Black)
+                            .Text(e => e.Path("Title"))
+                            .AlignBottom()
+                            .Column(1),
 
-                                    new Label()
-                                    .FontAttributes(Bold)
-                                    .TextColor(Black)
-                                    .Text(e => e.Path("Title"))
-                                    .AlignBottom()
-                                    .Column(1),
+                            new Label()
+                            .TextColor(DarkGray)
+                            .Text(e => e.Path("Description"))
+                            .FontSize(12)
+                            .FontAttributes(Italic)
+                            .AlignTop()
+                            .Column(1)
+                            .Row(1),
 
-                                    new Label()
-                                    .TextColor(DarkGray)
-                                    .Text(e => e.Path("Description"))
-                                    .FontSize(12)
-                                    .FontAttributes(Italic)
-                                    .AlignTop()
-                                    .Column(1)
-                                    .Row(1),
+                            new Label()
+                            .Text(e => e.Path("Date"))
+                            .TextColor(DarkGray)
+                            .FontSize(10)
+                            .Column(2)
+                            .AlignBottomEnd(),
 
-                                    new Label()
-                                    .Text(e => e.Path("Date"))
-                                    .TextColor(DarkGray)
-                                    .FontSize(10)
-                                    .Column(2)
-                                    .AlignBottomEnd(),
-
-                                    new Label()
-                                    .Text(e => e.Path("Amount"))
-                                    .TextColor(e => e.Path("Color"))
-                                    .FontSize(12)
-                                    .Column(2)
-                                    .Row(1)
-                                    .AlignTopEnd()
-                                )
-                            )
+                            new Label()
+                            .Text(e => e.Path("Amount"))
+                            .TextColor(e => e.Path("Color"))
+                            .FontSize(12)
+                            .Column(2)
+                            .Row(1)
+                            .AlignTopEnd()
                         )
                     )
                 ),
 
-                new TabViewItem()
-                .HeaderText("Gelirler")
-                .HeaderTextColor(Black)
-                .HeaderFontAttributes(Bold)
+                new DXPopup()
+                .IsOpen(e => e.Path("IsShowPopup"))
+                .RowSpan(2)
+                .AnimationDuration(new TimeSpan(0, 0, 0, 1))
+                .VerticalAlignment(PopupVerticalAlignment.Center)
+                .HorizontalAlignment(PopupHorizontalAlignment.Center)
+                .AllowShadow(true)
+                .AllowScrim(false)
+                .ShadowHorizontalOffset(20)
+                .ShadowVerticalOffset(20)
+                .CornerRadius(20)
+                .ShadowRadius(20)
+                .ShadowColor(Gray)
                 .Content(
                     new Grid()
-                    .RowDefinitions(e => e.Star(.7).Star(9.3))
-                    .Spacing(10)
+                    .WidthRequest(250)
+                    .HeightRequest(200)
+                    .Padding(15)
                     .Margin(10)
+                    .RowDefinitions(e => e.Absolute(50).Absolute(50).Absolute(50))
+                    .RowSpacing(10)
                     .Children(
                         new ComboBoxEdit()
-                        .SelectedIndex(e => e.Path("InIType"))
-                        .SelectionChangedCommand(e => e.Path("InItemTypeChangedCommand"))
+                        .SelectedIndex(e => e.Path("OperationType"))
+                        .ItemsSource(new List<string>
+                        {
+                            "Tümü",
+                            "Gelir",
+                            "Gider"
+                        }),
+
+                        new ComboBoxEdit()
+                        .SelectedIndex(e => e.Path("DateType"))
+                        .Row(1)
                         .ItemsSource(new List<string>
                         {
                             "Son 1 Haftalık Veri",
@@ -125,164 +146,11 @@ public partial class ItemsPage(ItemsPageViewModel viewModel) : FmgLibContentPage
                             "Son 1 Yıllık Veri"
                         }),
 
-                        new ShimmerView()
-                        .WaveWidth(0.7)
-                        .WaveOpacity(0.8)
-                        .WaveDuration(new TimeSpan(0, 0, 0, 1))
-                        .IsLoading(e => e.Path("IsLoadingInItems"))
-                        .Row(1)
-                        .LoadingView(
-                            new DXCollectionView()
-                            .ItemsSource(e => e.Path("LoadingItems"))
-                            .IsScrollBarVisible(false)
-                            .ItemTemplate(() =>
-                                new Border()
-                                .Margin(5, 5)
-                                .BackgroundColor(LightGray)
-                                .StrokeShape(new RoundRectangle().CornerRadius(25))
-                                .HeightRequest(60)
-                                .StrokeThickness(0)
-                            )
-                        )
-                        .Content(
-                            new DXCollectionView()
-                            .ItemsSource(e => e.Path("InItems"))
-                            .ItemTemplate(() =>
-                                new Grid()
-                                .RowDefinitions(e => e.Star().Star())
-                                .ColumnDefinitions(e => e.Star(1).Star(6).Star(3))
-                                .Spacing(5)
-                                .Margin(5)
-                                .Children(
-                                    new DXImage()
-                                    .Source(e => e.Path("Icon"))
-                                    .SizeRequest(30, 30)
-                                    .RowSpan(2),
-
-                                    new Label()
-                                    .FontAttributes(Bold)
-                                    .TextColor(Black)
-                                    .Text(e => e.Path("Title"))
-                                    .AlignBottom()
-                                    .Column(1),
-
-                                    new Label()
-                                    .TextColor(DarkGray)
-                                    .Text(e => e.Path("Description"))
-                                    .FontSize(12)
-                                    .FontAttributes(Italic)
-                                    .AlignTop()
-                                    .Column(1)
-                                    .Row(1),
-
-                                    new Label()
-                                    .Text(e => e.Path("Date"))
-                                    .TextColor(DarkGray)
-                                    .FontSize(10)
-                                    .Column(2)
-                                    .AlignBottomEnd(),
-
-                                    new Label()
-                                    .Text(e => e.Path("Amount"))
-                                    .TextColor(e => e.Path("Color"))
-                                    .FontSize(12)
-                                    .Column(2)
-                                    .Row(1)
-                                    .AlignTopEnd()
-                                )
-                            )
-                        )
-                    )
-                ),
-
-                new TabViewItem()
-                .HeaderText("Giderler")
-                .HeaderTextColor(Black)
-                .HeaderFontAttributes(Bold)
-                .Content(
-                    new Grid()
-                    .RowDefinitions(e => e.Star(.7).Star(9.3))
-                    .Spacing(10)
-                    .Margin(10)
-                    .Children(
-                        new ComboBoxEdit()
-                        .SelectedIndex(e => e.Path("OutIType"))
-                        .SelectionChangedCommand(e => e.Path("OutItemTypeChangedCommand"))
-                        .ItemsSource(new List<string>
-                        {
-                            "Son 1 Haftalık Veri",
-                            "Son 1 Aylık Veri",
-                            "Son 6 Aylık Veri",
-                            "Son 1 Yıllık Veri"
-                        }),
-
-                        new ShimmerView()
-                        .WaveWidth(0.7)
-                        .WaveOpacity(0.8)
-                        .WaveDuration(new TimeSpan(0, 0, 0, 1))
-                        .IsLoading(e => e.Path("IsLoadingOutItems"))
-                        .Row(1)
-                        .LoadingView(
-                            new DXCollectionView()
-                            .ItemsSource(e => e.Path("LoadingItems"))
-                            .IsScrollBarVisible(false)
-                            .ItemTemplate(() =>
-                                new Border()
-                                .Margin(5, 5)
-                                .BackgroundColor(LightGray)
-                                .StrokeShape(new RoundRectangle().CornerRadius(25))
-                                .HeightRequest(60)
-                                .StrokeThickness(0)
-                            )
-                        )
-                        .Content(
-                            new DXCollectionView()
-                            .ItemsSource(e => e.Path("OutItems"))
-                            .ItemTemplate(() =>
-                                new Grid()
-                                .RowDefinitions(e => e.Star().Star())
-                                .ColumnDefinitions(e => e.Star(1).Star(6).Star(3))
-                                .Spacing(5)
-                                .Margin(5)
-                                .Children(
-                                    new DXImage()
-                                    .Source(e => e.Path("Icon"))
-                                    .SizeRequest(30, 30)
-                                    .RowSpan(2),
-
-                                    new Label()
-                                    .FontAttributes(Bold)
-                                    .TextColor(Black)
-                                    .Text(e => e.Path("Title"))
-                                    .AlignBottom()
-                                    .Column(1),
-
-                                    new Label()
-                                    .TextColor(DarkGray)
-                                    .Text(e => e.Path("Description"))
-                                    .FontSize(12)
-                                    .FontAttributes(Italic)
-                                    .AlignTop()
-                                    .Column(1)
-                                    .Row(1),
-
-                                    new Label()
-                                    .Text(e => e.Path("Date"))
-                                    .TextColor(DarkGray)
-                                    .FontSize(10)
-                                    .Column(2)
-                                    .AlignBottomEnd(),
-
-                                    new Label()
-                                    .Text(e => e.Path("Amount"))
-                                    .TextColor(e => e.Path("Color"))
-                                    .FontSize(12)
-                                    .Column(2)
-                                    .Row(1)
-                                    .AlignTopEnd()
-                                )
-                            )
-                        )
+                        new Button()
+                        .Text("Filtrele")
+                        .FontAttributes(Bold)
+                        .Command(e => e.Path("ApplyFilterCommand"))
+                        .Row(2)
                     )
                 )
             )

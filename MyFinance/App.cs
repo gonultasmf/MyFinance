7ee785ab6fd78800a9cffb6ff9d63512d0b800e7
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-12-02 08:59:16 +0300

    CreateWindow override in App.cs #1

--- MyFinance/App.cs
+++ MyFinance/App.cs
@@ -3,10 +3,18 @@
 public partial class App : Application
 {
     public static string CurrentUserId = string.Empty;
+
+    private readonly IServiceProvider _service;
+
     public App(IServiceProvider services)
     {
+        _service = services;
         this
-        .Resources(AppStyles.Default)
-        .MainPage(services.GetService<AppShell>());
+        .Resources(AppStyles.Default);
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(_service.GetService<AppShell>());
     }
 }
